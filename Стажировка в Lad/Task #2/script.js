7aeb52e64d907ac4e4e608e7c343d9d48fa7be95
IamSkezy <86077283+IamSkezy@users.noreply.github.com>
2022-10-27 23:58:22 +0300

    Delete Стажировка в Lad directory

--- Стажировка в Lad/Task #2/script.js
+++
@@ -1,5 +0,0 @@
-setTimeout(() => {
-    let formElement = document.forms['formElement'];
-    formElement.addEventListener("focusin", () => event.target.classList.add('focused'));
-    formElement.addEventListener("focusout", () => event.target.classList.remove('focused'));
-}, 500);
